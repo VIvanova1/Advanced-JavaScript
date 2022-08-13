bb137ab2ee6a4f66abbdb9f5c10fc5e5062fdbeb
Veneta Ivanova <i.veneta.v@gmail.com>
2022-08-13 16:52:46 +0300

    Create 09.squareOfStars.js

---
+++ Syntax, Functions and Statements/Lab/09.squareOfStars.js
@@ -0,0 +1,26 @@
+function squareOfStars(counting) {
+    if (Number(counting) === isNaN()) {
+        counting = 5
+    }
+    for (let i = 0; i < counting; i++) {
+        console.log('*'.repeat(counting));
+    }
+}
+//---Test---
+squareOfStars(5);
+
+/* Result
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+ */
+
+//---Test---
+squareOfStars(2);
+
+/* Result
+ * *
+ * *
+ */
