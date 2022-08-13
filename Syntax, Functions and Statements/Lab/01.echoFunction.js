016a9096b2b9f6751f445b8f58542192c8148627
Veneta Ivanova <i.veneta.v@gmail.com>
2022-08-13 15:19:02 +0300

    Update 01.echoFunction.js

---
+++ Syntax, Functions and Statements/Lab/01.echoFunction.js
@@ -0,0 +1,10 @@
+function echoFunction(string) {
+    console.log(string.length);
+    console.log(string);
+}
+//---Test---
+echoFunction('Hello, JavaScript!')
+    /* Result
+    18
+    Hello, JavaScript!
+     */
