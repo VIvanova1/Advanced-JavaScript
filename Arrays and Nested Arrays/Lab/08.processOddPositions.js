85f6e320035a0771e8dce35ce52d639f83bee9b2
Veneta Ivanova <i.veneta.v@gmail.com>
2022-08-14 15:26:35 +0300

    Update 08.processOddPositions.js

--- Arrays and Nested Arrays/Lab/08.processOddPositions.js
+++ Arrays and Nested Arrays/Lab/08.processOddPositions.js
@@ -1,28 +1,26 @@
-function processOddPositions(array) {
-    // const result = [];
-    // for (let i = 0; i < array.length; i++) {
-    //     if (i % 2 !== 0) {
-    //         result.push(Number(array[i]) * 2)
-    //     }
-    // }
-    // let reversedArray = result.reverse();
-    // return reversedArray;
+//function processOddPositions(array) {
+// const odd = array.filter((x, i) => i % 2);
+// const doubled = odd.map(x => x * 2);
+// doubled.reverse();
+// return doubled;
+//}
 
-    const odd = array.filter((x, i) => i % 2);
-    const doubled = odd.map(x => x * 2);
-    doubled.reverse();
-    return doubled;
-}
+const processOddPositions = array => array
+    .filter((x, i) => i % 2)
+    .map(x => x * 2)
+    .reverse()
+    .join(' ');
+
 
 console.log('---Test 1---');
-processOddPositions([10, 15, 20, 25])
-
-/* Result
-50 30
-*/
+console.log(processOddPositions([10, 15, 20, 25]))
+    /* Result
+    50 30
+    */
 
 console.log('---Test 2---');
-processOddPositions([3, 0, 10, 4, 7, 3])
+console.log(processOddPositions([3, 0, 10, 4, 7, 3]))
+''
 
 /* Result
 6 8 0
