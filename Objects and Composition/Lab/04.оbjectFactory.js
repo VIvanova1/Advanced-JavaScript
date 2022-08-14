9d4f72e59121433fa93fefb36104d6992bc17566
Veneta Ivanova <i.veneta.v@gmail.com>
2022-08-14 23:32:47 +0300

    Create 04.оbjectFactory.js

---
+++ Objects and Composition/Lab/04.оbjectFactory.js
@@ -0,0 +1,67 @@
+function factory(lib, list) {
+    return list.map(order => {
+        const object = Object.assign({}, order.template);
+
+        for (const part of order.parts) {
+            object[part] = lib[part]
+        }
+        return object
+
+    })
+
+
+}
+//'---Test 1---';
+const library = {
+    print: function() {
+        console.log(`${this.name} is printing a page`);
+    },
+    scan: function() {
+        console.log(`${this.name} is scanning a document`);
+    },
+    play: function(artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+};
+const orders = [{
+        template: { name: 'ACME Printer' },
+        parts: ['print']
+    },
+    {
+        template: { name: 'Initech Scanner' },
+        parts: ['scan']
+    },
+    {
+        template: { name: 'ComTron Copier' },
+        parts: ['scan', 'print']
+    },
+    {
+        template: { name: 'BoomBox Stereo' },
+        parts: ['play']
+    }
+];
+const products = factory(library, orders);
+console.log(products);
+
+/* Result
+[
+  {
+    name: 'ACME Printer',
+    print: [Function: print]
+  },
+  {
+    name: 'Initech Scanner',
+    scan: [Function: scan]
+  },
+  {
+    name: 'ComTron Copier',
+    scan: [Function: scan],
+    print: [Function: print]
+  },
+  {
+    name: 'BoomBox Stereo',
+    play: [Function: play]
+  }
+]
+
+*/
