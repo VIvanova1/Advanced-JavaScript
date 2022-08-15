6bec667108a4e171e310058d9b29423851f83ca1
Veneta Ivanova <i.veneta.v@gmail.com>
2022-08-15 16:39:43 +0300

    Create SumNumbers.js

---
+++ DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,8 @@
+function calc() {
+    const num1 = document.getElementById('num1');
+    const num2 = document.getElementById('num2');
+
+    const result = Number(num1.value) + Number(num2.value);
+    document.getElementById('sum').value = result
+
+}
