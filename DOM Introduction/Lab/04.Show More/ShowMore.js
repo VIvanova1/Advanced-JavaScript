aeff34172b4aad43ed9288315117667d0daed3b4
Veneta Ivanova <i.veneta.v@gmail.com>
2022-08-15 16:56:12 +0300

    Update ShowMore.js

--- DOM Introduction/Lab/04.Show More/ShowMore.js
+++ DOM Introduction/Lab/04.Show More/ShowMore.js
@@ -2,8 +2,3 @@ function showText() {
     document.getElementById('text').style.display = 'inline';
     document.getElementById('more').style.display = 'none';
 }
-
-function showLess() {
-    document.getElementById('text').style.display = 'none';
-    document.getElementById('more').style.display = 'inline';
-}
