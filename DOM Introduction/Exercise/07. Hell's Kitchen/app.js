95f73ee0ceb777417b75b7ccd201dac7ffb32211
Veneta Ivanova <i.veneta.v@gmail.com>
2022-09-07 20:16:36 +0300

    Update app.js

--- DOM Introduction/Exercise/07. Hell's Kitchen/app.js
+++ DOM Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -1,53 +1,3 @@
-// function solve() {
-//     document.querySelector('#btnSend').addEventListener('click', onClick);
-
-//     function onClick() {
-//         const restaurants = {};
-
-//         const inputArea = document.querySelector('textarea').value;
-//         const input = inputArea.split('"');
-
-//         for (let i = 1; i < input.length; i += 2) {
-//             const [restaurantName, allWorkers] = input[i].split(' - ');
-
-//             workers = allWorkers.split(', ');
-//             let averageSalary = 0;
-//             for (const person of workers) {
-//                 let [personName, salary] = person.split(' ');
-
-//                 if (!restaurants.hasOwnProperty(restaurantName)) {
-//                     restaurants[restaurantName] = {};
-//                 }
-//                 restaurants[restaurantName][personName] = salary;
-
-//                 averageSalary += Number(salary)
-//             }
-//             averageSalary /= workers.length;
-//             restaurants[restaurantName].averageSalary = averageSalary;
-//         }
-
-
-//         let sortedRestaurants = Object.entries(restaurants).sort(([keyA, valueA], [keyB, valueB]) => {
-//             return valueB.averageSalary - valueA.averageSalary;
-//         });
-
-//         const bestRes = document.querySelector('div#bestRestaurant span');
-//         const bestResWorkes = document.querySelector('div#workers span');
-
-//         let workersObj = Object.entries(sortedRestaurants[0][1]);
-//         workersObj.pop()
-
-//         bestRes.textContent = `Name: ${sortedRestaurants[0][0]} Average Salary: ${(sortedRestaurants[0][1].averageSalary).toFixed(2)} Best Salary: ${Number(workersObj[0][1]).toFixed(2)}`
-
-//         bestResWorkes = []
-//         for (const person of workersObj) {
-//             bestResWorkes.push(`Name: ${person[0]} With Salary ${person[1]}`)
-//         }
-//         bestResWorkes.textContent = bestResWorkes.join(' ')
-//     }
-
-// }
-
 function solve() {
     document.querySelector('#btnSend').addEventListener('click', onClick);
 
@@ -87,9 +37,22 @@ function solve() {
             return valueB - valueA;
         });
 
-        let winnerSalary = sortWinner[0][1];
-        console.log(winnerSalary);
+        let winnerSalary = Number(sortWinner[0][1]);
 
+        let resultWinner = document.querySelector('#bestRestaurant span');
+        resultWinner.textContent = `Name: ${winnerName} Average Salary: ${winnerAvrgS} Best Salary: ${winnerSalary.toFixed(2)}`;
+
+
+        let res = '';
+        for (const winWorker of sortWinner) {
+            if (winWorker[0] === 'averageSalary') {
+                continue;
+            }
+
+            res += `Name: ${winWorker[0]} With Salary: ${winWorker[1]} `
+        }
+        let winnerWorkers = document.querySelector('#workers span');
+        winnerWorkers.textContent = res;
 
     }
 
