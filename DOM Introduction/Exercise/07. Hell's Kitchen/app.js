81b8df5e1d54fe0eae4a91f2fdd4a956f9538f3b
Veneta Ivanova <i.veneta.v@gmail.com>
2022-09-07 19:26:39 +0300

    Update app.js

--- DOM Introduction/Exercise/07. Hell's Kitchen/app.js
+++ DOM Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -1,49 +1,77 @@
+// function solve() {
+//     document.querySelector('#btnSend').addEventListener('click', onClick);
+
+//     function onClick() {
+//         const restaurants = {};
+
+//         const inputArea = document.querySelector('textarea').value;
+//         const input = inputArea.split('"');
+
+//         for (let i = 1; i < input.length; i += 2) {
+//             const [restaurantName, allWorkers] = input[i].split(' - ');
+
+//             workers = allWorkers.split(', ');
+//             let averageSalary = 0;
+//             for (const person of workers) {
+//                 let [personName, salary] = person.split(' ');
+
+//                 if (!restaurants.hasOwnProperty(restaurantName)) {
+//                     restaurants[restaurantName] = {};
+//                 }
+//                 restaurants[restaurantName][personName] = salary;
+
+//                 averageSalary += Number(salary)
+//             }
+//             averageSalary /= workers.length;
+//             restaurants[restaurantName].averageSalary = averageSalary;
+//         }
+
+
+//         let sortedRestaurants = Object.entries(restaurants).sort(([keyA, valueA], [keyB, valueB]) => {
+//             return valueB.averageSalary - valueA.averageSalary;
+//         });
+
+//         const bestRes = document.querySelector('div#bestRestaurant span');
+//         const bestResWorkes = document.querySelector('div#workers span');
+
+//         let workersObj = Object.entries(sortedRestaurants[0][1]);
+//         workersObj.pop()
+
+//         bestRes.textContent = `Name: ${sortedRestaurants[0][0]} Average Salary: ${(sortedRestaurants[0][1].averageSalary).toFixed(2)} Best Salary: ${Number(workersObj[0][1]).toFixed(2)}`
+
+//         bestResWorkes = []
+//         for (const person of workersObj) {
+//             bestResWorkes.push(`Name: ${person[0]} With Salary ${person[1]}`)
+//         }
+//         bestResWorkes.textContent = bestResWorkes.join(' ')
+//     }
+
+// }
+
 function solve() {
     document.querySelector('#btnSend').addEventListener('click', onClick);
 
     function onClick() {
-        const restaurants = {};
 
-        const inputArea = document.querySelector('textarea').value;
-        const input = inputArea.split('"');
+        let input = JSON.parse(document.querySelector("textarea").value);
 
-        for (let i = 1; i < input.length; i += 2) {
-            const [restaurantName, allWorkers] = input[i].split(' - ');
+        let averageSalary = 0;
+        let restaurants = {};
+        for (const line of input) {
+            let [restaurantName, workers] = line.split(' - ');
 
-            workers = allWorkers.split(', ');
-            let averageSalary = 0;
-            for (const person of workers) {
-                let [personName, salary] = person.split(' ');
+            let workerInfo = workers.split(', ')
 
+            for (const worker of workerInfo) {
+                let [name, salary] = worker.split(' ')
                 if (!restaurants.hasOwnProperty(restaurantName)) {
                     restaurants[restaurantName] = {};
                 }
-                restaurants[restaurantName][personName] = salary;
+                restaurants[restaurantName][name] = salary;
 
-                averageSalary += Number(salary)
             }
-            averageSalary /= workers.length;
-            restaurants[restaurantName].averageSalary = averageSalary;
+
+
         }
-
-
-        let sortedRestaurants = Object.entries(restaurants).sort(([keyA, valueA], [keyB, valueB]) => {
-            return valueB.averageSalary - valueA.averageSalary;
-        });
-
-        const bestRes = document.querySelector('div#bestRestaurant span');
-        const bestResWorkes = document.querySelector('div#workers span');
-
-        let workersObj = Object.entries(sortedRestaurants[0][1]);
-        workersObj.pop()
-
-        bestRes.textContent = `Name: ${sortedRestaurants[0][0]} Average Salary: ${(sortedRestaurants[0][1].averageSalary).toFixed(2)} Best Salary: ${Number(workersObj[0][1]).toFixed(2)}`
-
-        bestResWorkesResult = []
-        for (const person of workersObj) {
-            bestResWorkesResult.push(`Name: ${person[0]} With Salary ${person[1]}`)
-        }
-        bestResWorkes.textContent = bestResWorkesResult.join(' ')
     }
-
 }
