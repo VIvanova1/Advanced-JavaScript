2a4ab1f186774c91d2c30d44107b9b5cc9ec165e
Veneta Ivanova <i.veneta.v@gmail.com>
2022-09-11 21:42:27 +0300

    Create app.js

---
+++ DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
@@ -0,0 +1,17 @@
+function focused() {
+    const inputElements = document.querySelectorAll('input');
+
+    for (const el of inputElements) {
+        const divElementClass = el.parentNode;
+        el.addEventListener('focus', (e) => {
+
+            divElementClass.className = 'focused';
+        });
+
+        el.addEventListener('blur', (e) => {
+            divElementClass.className = ' ';
+        })
+    }
+
+
+}
