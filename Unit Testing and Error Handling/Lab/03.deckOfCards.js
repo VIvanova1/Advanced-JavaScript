f896054cdac3219d941f2fdbbb541b88006589ed
Veneta Ivanova <i.veneta.v@gmail.com>
2022-09-17 14:43:06 +0300

    fix 'invalid'

--- Unit Testing and Error Handling/Lab/03.deckOfCards.js
+++ Unit Testing and Error Handling/Lab/03.deckOfCards.js
@@ -10,7 +10,7 @@ function deckOfCards(cards) {
             result.push(newCard);
 
         } catch (err) {
-            result = ['Ivanlid card: ' + card]
+            result = ['Invalid card: ' + card]
         }
     }
     console.log(result.join(' '));
@@ -18,16 +18,21 @@ function deckOfCards(cards) {
     function playingCards(face, suit) {
 
         const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-        if (faces.indexOf(face) === -1) {
-            throw new RangeError('Invalid face: ' + face)
-        }
-
         const suits = {
             'S': '\u2660',
             'H': '\u2665',
             'D': '\u2666',
             'C': '\u2663',
         }
+
+        if (faces.indexOf(face) === -1) {
+            throw new RangeError('Invalid face: ' + face)
+        }
+        if (suits[suit] === undefined) {
+            throw new RangeError('Invalid suit: ' + suit)
+        }
+
+
 
 
         const result = {
