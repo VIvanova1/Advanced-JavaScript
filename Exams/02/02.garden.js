0cb086498c01106ad4961e5779e23dbf43131985
Veneta Ivanova <i.veneta.v@gmail.com>
2022-10-18 20:30:21 +0300

    garden

--- Exams/02/02.garden.js
+++ Exams/02/02.garden.js
@@ -81,16 +81,3 @@ class Garden {
     }
 
 }
-const myGarden = new Garden(250)
-
-console.log(myGarden.addPlant('apple', 20));
-
-console.log(myGarden.addPlant('orange', 100));
-
-console.log(myGarden.addPlant('cucumber', 30));
-
-console.log(myGarden.ripenPlant('apple', 10));
-
-console.log(myGarden.ripenPlant('orange', 1));
-
-console.log(myGarden.ripenPlant('olive', 30));
